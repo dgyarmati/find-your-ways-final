dac6549b7648a6d5916d76fe441c0f803cd7db68
co-NaN <dgyarmati@protonmail.com>
2019-06-02 12:53:13 +0200

    Added fixed header, carousel.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function showSlides(idx) {
-    let backgrounds = ["url(assets/images/drawing.jpg)", "url(assets/images/at_night.jpg)"];
+    let backgrounds = ["url(assets/images/drawing.jpg)", "url(assets/images/at_night.jpg)", "url(assets/images/budapest_wheel.jpg)", "url(assets/images/drawing3.jpg)", "url(assets/images/drawing_panorama.jpg)"];
     document.getElementById('slide').style.backgroundImage = backgrounds[idx++];
-    if (idx === 2) {
+    if (idx === backgrounds.length) {
         idx = 0;
     }
-    setTimeout(showSlides, 6000, idx);
+    setTimeout(showSlides, 4000, idx);
 }
 
 showSlides(0);
